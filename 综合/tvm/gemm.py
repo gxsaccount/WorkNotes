ec6398591c9610e12bfc0d7737039a94aae0de57
theWeirdCode <447653585@qq.com>
2021-03-22 20:42:04 +0800

    Create gemm.py

---
+++ 综合/tvm/gemm.py
@@ -0,0 +1,484 @@
+'''
+from:
+https://tvm.apache.org/docs/tutorials/optimize/opt_gemm.html#sphx-glr-tutorials-optimize-opt-gemm-py
+tvm的gemm优化是这里的子集https://github.com/flame/how-to-optimize-gemm
+
+两种优化cpu上计算密集型任务的重要方法  
+1.增加cache命中率
+2.SIMD(Single instruction multi-data),tvm 使用llvm进行循环的优化，需要我们将循环的转化为统一的形式  
+
+补充：将乘法转化为加法，Winograd算法
+'''
+# The size of the matrix
+# (M, K) x (K, N)
+# You are free to try out different shapes, sometimes TVM optimization outperforms numpy with MKL.
+
+'''
+MKL:Math Kernel Library ，因特尔数学库 
+在不同的shape影响下，tvm有时会使用因特尔的数学库
+'''
+import tvm
+import tvm.testing
+from tvm import te
+import numpy
+'''timeit-- 准确测量小段代码的执行时间'''
+import timeit
+M = 1024
+K = 1024
+N = 1024
+
+# The default tensor type in tvm
+dtype = "float32"
+
+# using Intel AVX2(Advanced Vector Extensions) ISA for SIMD
+# To get the best performance, please change the following line
+# to llvm -mcpu=core-avx2, or specific type of CPU you use
+'''
+最好的优化需要指定llvm运行的平台,例如llvm -mcpu=core-avx2
+'''
+target = "llvm"
+ctx = tvm.context(target, 0)
+
+# Random generated tensor for testing
+a = tvm.nd.array(numpy.random.rand(M, K).astype(dtype), ctx)
+b = tvm.nd.array(numpy.random.rand(K, N).astype(dtype), ctx)
+
+####################################################################################################
+'''numpy的耗时'''
+np_repeat = 100
+np_runing_time = timeit.timeit(
+    setup="import numpy\n"
+    "M = " + str(M) + "\n"
+    "K = " + str(K) + "\n"
+    "N = " + str(N) + "\n"
+    'dtype = "float32"\n'
+    "a = numpy.random.rand(M, K).astype(dtype)\n"
+    "b = numpy.random.rand(K, N).astype(dtype)\n",
+    stmt="answer = numpy.dot(a, b)",
+    number=np_repeat,
+)
+print("Numpy running time: %f" % (np_runing_time / np_repeat))
+'''\ numpy的耗时'''
+
+####################################################################################################
+'''baseline'''
+answer = numpy.dot(a.asnumpy(), b.asnumpy())
+
+# Algorithm
+k = te.reduce_axis((0, K), "k")
+A = te.placeholder((M, K), name="A")
+B = te.placeholder((K, N), name="B")
+C = te.compute((M, N), lambda x, y: te.sum(A[x, k] * B[k, y], axis=k), name="C")
+
+# Default schedule
+schedule = te.create_schedule(C.op)
+func = tvm.build(schedule, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=1)
+print("Baseline: %f" % evaluator(a, b, c).mean)
+
+'''打印基于baseline生成的中间层语言'''  
+print(tvm.lower(schedule, [A, B, C], simple_mode=True))
+
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  for (x: int32, 0, 1024) {
+    for (y: int32, 0, 1024) {
+      C_2[((x*1024) + y)] = 0f32
+      for (k: int32, 0, 1024) {
+        C_2[((x*1024) + y)] = ((float32*)C_2[((x*1024) + y)] + ((float32*)A_2[((x*1024) + k)]*(float32*)B_2[((k*1024) + y)]))
+      }
+    }
+  }
+}
+'''
+####################################################################################################
+'''
+分块优化，利用数据的局部访问性
+''' 
+'''该例子使用32*32的块进行分块,cache大小有限，外层循环会出现较多miss'''  
+bn = 32
+schedule = te.create_schedule(C.op)
+# Blocking by loop tiling
+xo, yo, xi, yi = schedule[C].tile(C.op.axis[0], C.op.axis[1], bn, bn)
+(k,) = schedule[C].op.reduce_axis
+ko, ki = schedule[C].split(k, factor=4)
+
+# Hoist reduction domain outside the blocking loop
+schedule[C].reorder(xo, yo, ko, ki, xi, yi)
+
+func = tvm.build(schedule, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+# By simply tiling the loop 32x32, and hoisting ko, ki outside the blocking loops,
+# we can see big speedup compared with the baseline.
+evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
+print("Opt1: %f" % evaluator(a, b, c).mean)
+print(tvm.lower(schedule, [A, B, C], simple_mode=True))
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  for (x.outer: int32, 0, 32) {
+    for (y.outer: int32, 0, 32) {
+      for (x.inner.init: int32, 0, 32) {
+        for (y.inner.init: int32, 0, 32) {
+          C_2[((((x.outer*32768) + (x.inner.init*1024)) + (y.outer*32)) + y.inner.init)] = 0f32
+        }
+      }
+      for (k.outer: int32, 0, 256) {
+        for (k.inner: int32, 0, 4) {
+          for (x.inner: int32, 0, 32) {
+            for (y.inner: int32, 0, 32) {
+              C_2[((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)) + y.inner)] = ((float32*)C_2[((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)) + y.inner)] + ((float32*)A_2[((((x.outer*32768) + (x.inner*1024)) + (k.outer*4)) + k.inner)]*(float32*)B_2[((((k.outer*4096) + (k.inner*1024)) + (y.outer*32)) + y.inner)]))
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+
+###################################################################################################
+'''Vectorization(例如SIMD)'''
+'''矢量化优化：当内存访问模式统一时，编译器可以检测到该模式并将连续内存传递给矢量处理器。'''  
+s = te.create_schedule(C.op)
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], bn, bn)
+(k,) = s[C].op.reduce_axis
+ko, ki = s[C].split(k, factor=4)
+
+s[C].reorder(xo, yo, ko, ki, xi, yi)
+
+# Vectorization
+s[C].vectorize(yi)
+
+func = tvm.build(s, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
+print("Opt2: %f" % evaluator(a, b, c).mean)
+
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  for (x.outer: int32, 0, 32) {
+    for (y.outer: int32, 0, 32) {
+      for (x.inner.init: int32, 0, 32) {
+        C_2[ramp((((x.outer*32768) + (x.inner.init*1024)) + (y.outer*32)), 1, 32)] = broadcast(0f32, 32)
+      }
+      for (k.outer: int32, 0, 256) {
+        for (k.inner: int32, 0, 4) {
+          for (x.inner: int32, 0, 32) {
+            C_2[ramp((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)), 1, 32)] = ((float32x32*)C_2[ramp((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.inner*1024)) + (k.outer*4)) + k.inner)], 32)*(float32x32*)B_2[ramp((((k.outer*4096) + (k.inner*1024)) + (y.outer*32)), 1, 32)]))
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+####################################################################
+'''
+Loop Permutation
+改变循环的排列
+如果我们看一下上面的IR，我们可以看到内部循环行数据已向量化，并且B被转换为PackedB。 PackedB的遍历现在是顺序的。因此，我们将看一下A的访问模式。在当前调度中，
+A是逐列访问的，这不是缓存友好的。如果我们更改ki和内轴xi的嵌套循环顺序，则A矩阵的访问模式将更易于缓存。
+'''
+s = te.create_schedule(C.op)
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], bn, bn)
+(k,) = s[C].op.reduce_axis
+ko, ki = s[C].split(k, factor=4)
+
+'''改变了xi和ki的顺序'''
+# re-ordering
+s[C].reorder(xo, yo, ko, xi, ki, yi)
+s[C].vectorize(yi)
+
+func = tvm.build(s, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
+print("Opt3: %f" % evaluator(a, b, c).mean)
+
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  for (x.outer: int32, 0, 32) {
+    for (y.outer: int32, 0, 32) {
+      for (x.inner.init: int32, 0, 32) {
+        C_2[ramp((((x.outer*32768) + (x.inner.init*1024)) + (y.outer*32)), 1, 32)] = broadcast(0f32, 32)
+      }
+      for (k.outer: int32, 0, 256) {
+        for (x.inner: int32, 0, 32) {
+          for (k.inner: int32, 0, 4) {
+            C_2[ramp((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)), 1, 32)] = ((float32x32*)C_2[ramp((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.inner*1024)) + (k.outer*4)) + k.inner)], 32)*(float32x32*)B_2[ramp((((k.outer*4096) + (k.inner*1024)) + (y.outer*32)), 1, 32)]))
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+##################################################################################################################
+'''Array Packing
+This trick is to reorder the storage dimension of the array to convert the continuous access pattern on certain dimension to a sequential pattern after flattening.
+'''
+# We have to re-write the algorithm slightly.
+packedB = te.compute((N / bn, K, bn), lambda x, y, z: B[y, x * bn + z], name="packedB")
+C = te.compute(
+    (M, N),
+    lambda x, y: te.sum(A[x, k] * packedB[y // bn, k, tvm.tir.indexmod(y, bn)], axis=k),
+    name="C",
+)
+
+s = te.create_schedule(C.op)
+
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], bn, bn)
+(k,) = s[C].op.reduce_axis
+ko, ki = s[C].split(k, factor=4)
+
+s[C].reorder(xo, yo, ko, xi, ki, yi)
+s[C].vectorize(yi)
+
+x, y, z = s[packedB].op.axis
+s[packedB].vectorize(z)
+s[packedB].parallel(x)
+
+func = tvm.build(s, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
+print("Opt4: %f" % evaluator(a, b, c).mean)
+
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  attr [packedB: Pointer(float32x32)] "storage_scope" = "global";
+  allocate(packedB, float32x32, [32768]) {
+    for (x: int32, 0, 32) "parallel" {
+      for (y: int32, 0, 1024) {
+        packedB[ramp(((x*32768) + (y*32)), 1, 32)] = (float32x32*)B_2[ramp(((y*1024) + (x*32)), 1, 32)]
+      }
+    }
+    for (x.outer: int32, 0, 32) {
+      for (y.outer: int32, 0, 32) {
+        for (x.inner.init: int32, 0, 32) {
+          C_2[ramp((((x.outer*32768) + (x.inner.init*1024)) + (y.outer*32)), 1, 32)] = broadcast(0f32, 32)
+        }
+        for (k.outer: int32, 0, 256) {
+          for (x.inner: int32, 0, 32) {
+            for (k.inner: int32, 0, 4) {
+              C_2[ramp((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)), 1, 32)] = ((float32x32*)C_2[ramp((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.inner*1024)) + (k.outer*4)) + k.inner)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + (k.inner*32)), 1, 32)]))
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'''
+##################################################################################
+'''
+Write cache for blocks
+blocking后，程序将结果逐块写入C，但访问方式不是顺序的。因此，我们可以使用顺序缓存阵列来保存块结果，并在所有块结果准备好后写入
+'''
+s = te.create_schedule(C.op)
+
+# Allocate write cache
+CC = s.cache_write(C, "global")
+
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], bn, bn)
+
+# Write cache is computed at yo
+s[CC].compute_at(s[C], yo)
+
+# New inner axes
+xc, yc = s[CC].op.axis
+
+(k,) = s[CC].op.reduce_axis
+ko, ki = s[CC].split(k, factor=4)
+s[CC].reorder(ko, xc, ki, yc)
+s[CC].unroll(ki)
+s[CC].vectorize(yc)
+
+x, y, z = s[packedB].op.axis
+s[packedB].vectorize(z)
+s[packedB].parallel(x)
+
+func = tvm.build(s, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
+print("Opt5: %f" % evaluator(a, b, c).mean)
+
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  attr [packedB: Pointer(float32x32)] "storage_scope" = "global";
+  allocate(packedB, float32x32, [32768]);
+  attr [C.global: Pointer(float32)] "storage_scope" = "global";
+  allocate(C.global, float32, [1024]) {
+    for (x: int32, 0, 32) "parallel" {
+      for (y: int32, 0, 1024) {
+        packedB[ramp(((x*32768) + (y*32)), 1, 32)] = (float32x32*)B_2[ramp(((y*1024) + (x*32)), 1, 32)]
+      }
+    }
+    for (x.outer: int32, 0, 32) {
+      for (y.outer: int32, 0, 32) {
+        for (x.c.init: int32, 0, 32) {
+          C.global[ramp((x.c.init*32), 1, 32)] = broadcast(0f32, 32)
+        }
+        for (k.outer: int32, 0, 256) {
+          for (x.c: int32, 0, 32) {
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[(((x.outer*32768) + (x.c*1024)) + (k.outer*4))], 32)*(float32x32*)packedB[ramp(((y.outer*32768) + (k.outer*128)), 1, 32)]))
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.c*1024)) + (k.outer*4)) + 1)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + 32), 1, 32)]))
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.c*1024)) + (k.outer*4)) + 2)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + 64), 1, 32)]))
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.c*1024)) + (k.outer*4)) + 3)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + 96), 1, 32)]))
+          }
+        }
+        for (x.inner: int32, 0, 32) {
+          for (y.inner: int32, 0, 32) {
+            C_2[((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)) + y.inner)] = (float32*)C.global[((x.inner*32) + y.inner)]
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+'''
+Parallel
+we can also utilize multi-core processors to do the thread-level parallelization.
+'''
+s = te.create_schedule(C.op)
+
+CC = s.cache_write(C, "global")
+
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], bn, bn)
+
+s[CC].compute_at(s[C], yo)
+
+xc, yc = s[CC].op.axis
+
+(k,) = s[CC].op.reduce_axis
+ko, ki = s[CC].split(k, factor=4)
+s[CC].reorder(ko, xc, ki, yc)
+s[CC].unroll(ki)
+s[CC].vectorize(yc)
+
+# parallel
+s[C].parallel(xo)
+
+x, y, z = s[packedB].op.axis
+s[packedB].vectorize(z)
+s[packedB].parallel(x)
+
+func = tvm.build(s, [A, B, C], target=target, name="mmult")
+assert func
+
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=50)
+opt6_time = evaluator(a, b, c).mean
+print("Opt6: %f" % opt6_time)
+
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+
+'''
+primfn(A_1: handle, B_1: handle, C_1: handle) -> ()
+  attr = {"global_symbol": "main", "tir.noalias": True}
+  buffers = {C: Buffer(C_2: Pointer(float32), float32, [1024, 1024], []),
+             B: Buffer(B_2: Pointer(float32), float32, [1024, 1024], []),
+             A: Buffer(A_2: Pointer(float32), float32, [1024, 1024], [])}
+  buffer_map = {A_1: A, B_1: B, C_1: C} {
+  attr [packedB: Pointer(float32x32)] "storage_scope" = "global";
+  allocate(packedB, float32x32, [32768]) {
+    for (x: int32, 0, 32) "parallel" {
+      for (y: int32, 0, 1024) {
+        packedB[ramp(((x*32768) + (y*32)), 1, 32)] = (float32x32*)B_2[ramp(((y*1024) + (x*32)), 1, 32)]
+      }
+    }
+    for (x.outer: int32, 0, 32) "parallel" {
+      attr [C.global: Pointer(float32)] "storage_scope" = "global";
+      allocate(C.global, float32, [1024]);
+      for (y.outer: int32, 0, 32) {
+        for (x.c.init: int32, 0, 32) {
+          C.global[ramp((x.c.init*32), 1, 32)] = broadcast(0f32, 32)
+        }
+        for (k.outer: int32, 0, 256) {
+          for (x.c: int32, 0, 32) {
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[(((x.outer*32768) + (x.c*1024)) + (k.outer*4))], 32)*(float32x32*)packedB[ramp(((y.outer*32768) + (k.outer*128)), 1, 32)]))
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.c*1024)) + (k.outer*4)) + 1)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + 32), 1, 32)]))
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.c*1024)) + (k.outer*4)) + 2)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + 64), 1, 32)]))
+            C.global[ramp((x.c*32), 1, 32)] = ((float32x32*)C.global[ramp((x.c*32), 1, 32)] + (broadcast((float32*)A_2[((((x.outer*32768) + (x.c*1024)) + (k.outer*4)) + 3)], 32)*(float32x32*)packedB[ramp((((y.outer*32768) + (k.outer*128)) + 96), 1, 32)]))
+          }
+        }
+        for (x.inner: int32, 0, 32) {
+          for (y.inner: int32, 0, 32) {
+            C_2[((((x.outer*32768) + (x.inner*1024)) + (y.outer*32)) + y.inner)] = (float32*)C.global[((x.inner*32) + y.inner)]
+          }
+        }
+      }
+    }
+  }
+}
+'''
